ecddcef6b6983b4d2656c6289a98ac3f641df832
Nited Webs <151686730+NitedWebs@users.noreply.github.com>
2024-03-03 12:31:41 -0300

    Delete Del Mar Restó directory

--- Del Mar Restó/scripts.js
+++
@@ -1,7 +0,0 @@
-// Código JavaScript para el menú desplegable
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-
-menuBtn.addEventListener('click', () => {
-  menu.classList.toggle('show-menu');
-});
